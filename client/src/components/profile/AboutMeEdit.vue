4cfd7f5e36c7381b26d813fe339c1d75f35a2e95
Jakub Jablonski <kuba.jablonski.zg@gmail.com>
2017-07-25 20:25:21 +0200

    Create AboutMeEdit component

--- client/src/components/profile/AboutMeEdit.vue
+++ client/src/components/profile/AboutMeEdit.vue
@@ -7,17 +7,49 @@
                 </div>
             </div>
             <div class="level-right">
-                <a>edit</a>
+                <a @click="$store.state.profile.edit.aboutMe = false" class="delete is-medium"></a>
             </div>
         </div>
         <br>
-        <p>Name</p>
-        <p>YOYOYO</p>
-        <br>
-        <p>Living in</p>
-        <p>BBAMBAM</p>
-        <br>
-        <p>Introduction</p>
-        <p>HEYA</p>
+        <div class="columns">
+            <div class="field column">
+                <label class="label">First Name</label>
+                <div class="control">
+                    <input class="input" type="text" placeholder="Text input">
+                </div>
+            </div>
+            <div class="field column">
+                <label class="label">Last Name</label>
+                <div class="control">
+                    <input class="input" type="text" placeholder="Text input">
+                </div>
+            </div>
+        </div>
+        <div class="field">
+            <label class="label">Living in</label>
+            <div class="control">
+                <input class="input" type="text" placeholder="Text input">
+            </div>
+        </div>
+        <div class="field">
+            <label class="label">Introduction</label>
+            <div class="control">
+                <textarea class="textarea" placeholder="Textarea"></textarea>
+            </div>
+        </div>
+        <div class="field is-grouped">
+            <div class="control">
+                <button class="button is-primary">Save</button>
+            </div>
+            <div class="control">
+                <button class="button is-link">Cancel</button>
+            </div>
+        </div>
     </div>
 </template>
+
+<style lang="scss" scoped>
+.columns {
+    margin-bottom: 0;
+}
+</style>
