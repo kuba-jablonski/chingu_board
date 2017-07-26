37550cd26eadad16b884356126e4eaa83d67ec5b
Jakub Jablonski <kuba.jablonski.zg@gmail.com>
2017-07-26 20:03:45 +0200

    Create fake projects module

---
+++ client/src/store/modules/projects.js
@@ -0,0 +1,19 @@
+const state = {
+    projects: [
+        {
+            details: {
+                name: 'Sample Project',
+                team: '2 people',
+                commitment: '3 h / day',
+                description: 'Lorem ipsum'
+            },
+            candidate: {
+                description: 'Lorem ipsum',
+                skills: {
+                    Javascript: 'Required',
+                    HTML: 'Optional'
+                }
+            }
+        }
+    ]
+}
