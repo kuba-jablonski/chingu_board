f5acf3716e0e02b0b2c33e950ba15e8bbf651177
Jakub Jablonski <kuba.jablonski.zg@gmail.com>
2017-07-28 19:39:58 +0200

    add some headings style

--- client/src/components/profile/Profile.vue
+++ client/src/components/profile/Profile.vue
@@ -1,7 +1,7 @@
 <template>
-    <section class="section container">
-        <div class="box">
-            <h2 class="title is-3">Your Profile</h2>
+    <section class="container">
+        <h2 id="profile-heading" class="title has-text-centered is-5">Your Profile</h2>
+        <div id="profile-section" class="box">
             <component :is="edit.aboutMe ? 'app-about-me-edit' : 'app-about-me'"></component>
             <component :is="edit.skills ? 'app-skills-edit' : 'app-skills'"></component>
             <component :is="edit.links ? 'app-links-edit' : 'app-links'"></component>
@@ -34,4 +34,30 @@ export default {
 }
 </script>
 
+<style lang="scss" scoped>
+@import '../../assets/styles/settings.scss';
+
+#profile-section {
+    margin: 2rem auto;
+}
+
+#profile-heading {
+    position: relative;
+    top: 2rem;
+    left: 2rem;
+    z-index: 10;
+    border-radius: 5px 5px 0 0;
+    background-color: white;
+    max-width: 300px;
+    border: 1px solid #D5D5D5;
+    border-bottom: none;
+    color: $primary;
+    font-weight: bold;
+    padding: 5px 0;
+    @media (max-width: $tablet) {
+        margin: 0 auto;
+        left: 0;
+    }
+}
+</style>
 
