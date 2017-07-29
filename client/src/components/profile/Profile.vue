d610967e3f4fa79397bc01ac85ad065aa40fc9f7
Jakub Jablonski <kuba.jablonski.zg@gmail.com>
2017-07-29 12:52:01 +0200

    Move box heading style to a file

--- client/src/components/profile/Profile.vue
+++ client/src/components/profile/Profile.vue
@@ -1,7 +1,7 @@
 <template>
     <section class="container">
-        <h2 id="profile-heading" class="title has-text-centered is-5">Your Profile</h2>
-        <div id="profile-section" class="box">
+        <h2 class="details-box-heading title has-text-centered is-5">Your Profile</h2>
+        <div class="details-box box">
             <component :is="edit.aboutMe ? 'app-about-me-edit' : 'app-about-me'"></component>
             <component :is="edit.skills ? 'app-skills-edit' : 'app-skills'"></component>
             <component :is="edit.links ? 'app-links-edit' : 'app-links'"></component>
@@ -35,29 +35,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import '../../assets/styles/settings.scss';
-
-#profile-section {
-    margin: 2rem auto;
-}
-
-#profile-heading {
-    position: relative;
-    top: 2rem;
-    left: 2rem;
-    z-index: 10;
-    border-radius: 5px 5px 0 0;
-    background-color: white;
-    max-width: 300px;
-    border: 1px solid #D5D5D5;
-    border-bottom: none;
-    color: $primary;
-    font-weight: bold;
-    padding: 5px 0;
-    @media (max-width: $tablet) {
-        margin: 0 auto;
-        left: 0;
-    }
-}
+@import '../../assets/styles/details-box.scss';
 </style>
 
