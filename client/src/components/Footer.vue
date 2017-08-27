7e543c18e25ee2d3eaa0459faf31e93502462030
Thomas Leroy <imtrip@gmail.com>
2017-08-27 18:53:25 +0200

    updated footer repo link

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -87,7 +87,7 @@
                     Build by <strong>Bears Team 11</strong>.
                 </p>
                 <p>
-                    <a class="icon" href="https://github.com/jgthms/bulma">
+                    <a class="icon" href="https://github.com/kuba-jablonski/chingu_board">
                         <i class="fa fa-github"></i>
                     </a>
                 </p>
